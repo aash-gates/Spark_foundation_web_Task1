198f83eee5302985bda6a5b7070c67c09585d092
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-09-15 01:20:30 +0530

    Create navbar.php

---
+++ php/navbar.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <style>
+    a:link {
+      color: white;
+    }
+
+    a:hover {
+      color: white;
+      text-decoration: none;
+    }
+  </style>
+</head>
+
+<body>
+  <!--Responsive Navbar-->
+  <nav>
+    <div class="container-fluid nav-wrapper #33691e light-green darken-4">
+      <a href="../index2.php" class="brand-logo"><img src="assets\images\Company Logo.png" style="height:60px;width:60px" /></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="../index2.php">Home</a></li>
+        <li><a href="transfermoney.php">View Customers</a></li>
+        <li><a href="transactionhistory.php">Transactions</a></li>
+      </ul>
+    </div>
+  </nav>
+  <!--end of navbar-->
+</body>
+
+</html>
